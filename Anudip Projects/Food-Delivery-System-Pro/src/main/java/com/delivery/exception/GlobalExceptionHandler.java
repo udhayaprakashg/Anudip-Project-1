12c19afb02d6eb09ddd7cb3478ec15086ef8f239
Udhayaprakash. G <112915485+udhayaprakashg@users.noreply.github.com>
2023-01-27 10:08:51 +0530

    Update GlobalExceptionHandler.java

--- Anudip Projects/Food-Delivery-System-Pro/src/main/java/com/delivery/exception/GlobalExceptionHandler.java
+++ Anudip Projects/Food-Delivery-System-Pro/src/main/java/com/delivery/exception/GlobalExceptionHandler.java
@@ -9,6 +9,8 @@ import java.util.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
